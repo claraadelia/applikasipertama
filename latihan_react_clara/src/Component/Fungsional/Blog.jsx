925031ce42d513f42931c5dbb383b8d395489744
clara adelia <claraadelia12@outlook.com>
2020-07-12 12:28:48 +0700

    menggunakan props di Reactjs

--- latihan_react_clara/src/Component/Fungsional/Blog.jsx
+++ latihan_react_clara/src/Component/Fungsional/Blog.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './CSS/Blog.css';
 
-const Blog =() => {
+const Blog =(props) => {
     return <div className="blog-wrap">
-        <img src="https://placeimg.com/640/480/tech" alt=""/>
-        <p>Tanggal Blog</p>
-        <p>Judul</p>
-        <p>Sumary Blog</p>
+        <img src="https://placeimg.com/620/460/tech" alt=""/>
+<p>{props.tanggal}</p>
+<p><b>{props.judul}</b></p>
+<p>{props.summary}</p>
 
     </div>
 
--- latihan_react_clara/src/Component/Fungsional/Home.jsx
+++ latihan_react_clara/src/Component/Fungsional/Home.jsx
@@ -3,8 +3,21 @@ import Blog from './Blog'
 
 const Home = () => {
     return <div>
-        <Blog/>
-         </div>
+        <Blog
+            tanggal="02 juni 2020"
+            judul="Teknologi Blockhain "
+            summary="There are many variations of passages of Lorem Ipsum available."/>
+
+        <Blog
+            tanggal="03 juni 2020"
+            judul="Teknologi Internet of things "
+            summary="There are many variations of passages of Lorem Ipsum available."/>
+        
+        <Blog
+            tanggal="04 juni 2020"
+            judul="Teknologi 5.0 "
+            summary="There are many variations of passages of Lorem Ipsum available."/>
+    </div>
 }
 
 export default Home;
