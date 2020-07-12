744694b49735fc004c3dc1550590d7fe964427f1
clara adelia <claraadelia12@outlook.com>
2020-07-12 11:56:28 +0700

    menambahkan gambar dan tulisan di blog

---
+++ latihan_react_clara/src/Component/Fungsional/Blog.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Blog =() => {
+    return <div className="blog-wrap">
+        <img src="https://placeimg.com/640/480/tech" alt=""/>
+        <p>Tanggal Blog</p>
+        <p>Judul</p>
+        <p>Sumary Blog</p>
+
+    </div>
+
+}
+
+export default Blog;
