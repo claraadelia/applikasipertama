921915870a3355b7698c7daaa6c4baed62fa4a29
clara adelia <claraadelia12@outlook.com>
2020-07-16 11:13:30 +0700

    berhasil menambahkan stock, dan memnuat tampilan

---
+++ latihan_react_clara/src/Component/Class/Produk.jsx
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+
+import './CSS/Produk.css';
+
+class Produk extends Component{
+    constructor(props) {
+        super(props)
+
+        this.state ={
+            stock: this.props.stock,
+            sub:"Beli",
+            Status:"Tersedia",
+            disabled : false
+        }
+    }
+
+    ButtonBeli = () => {
+        this.setState ({
+        stock: this.state.stock -1
+        })
+        if(this.state.stock ==1){
+            this.setState({
+                Status : "Habis",
+                disabled : true
+            })
+        }
+    }
+
+        render(){
+            return(
+
+                <div className="box-stock">
+                    <h2>{this.props.nama}</h2>
+                    <img src="https://tse4.mm.bing.net/th?id=OIP.rf428CRhPdRFDWUeLIaHMgHaEo&pid=Api&P=0&w=259&h=163" alt=""/>
+            <p>{this.props.harga}</p>
+                    <p>{this.state.stock}</p>
+                    <button className="btn-click" onClick={this.ButtonBeli} disabled={this.state.disabled}>Beli</button>
+                    <p>{this.state.Status}</p>
+
+
+                </div>
+            )
+                
+        }
+    }
+
+    export default Produk;
--- latihan_react_clara/src/Component/Fungsional/Home.jsx
+++ latihan_react_clara/src/Component/Fungsional/Home.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
-import Blog from './Blog'
+//import Blog from './Blog'
+import Produk from '../Class/Produk';
 
 const Home = () => {
     return <div>
-        <Blog
+        { <Produk nama="Macbook Pro 2020"stock="10" harga="33000000"/>
+        /*<Blog
             tanggal="02 juni 2020"
             judul="Teknologi Blockhain "
-            summary="There are many variations of passages of Lorem Ipsum available."/>
+            summary="There are many variations of passages of Lorem Ipsum available.
+            "/>
 
         <Blog
             tanggal="03 juni 2020"
-            judul="Teknologi Internet of things "
-            summary="There are many variations of passages of Lorem Ipsum available."/>
+            judul="Teknologi Internet of Things "
+            summary="There are many variations of passages of Lorem Ipsum available.
+            "/>
         
         <Blog
             tanggal="04 juni 2020"
             judul="Teknologi 5.0 "
-            summary="There are many variations of passages of Lorem Ipsum available."/>
+            summary="There are many variations of passages of Lorem Ipsum available.
+            "/>*/}
     </div>
 }
 
