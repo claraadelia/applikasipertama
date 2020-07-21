0ef734416cb724dbe3f310da68ed238c4fbbfb48
clara adelia <claraadelia12@outlook.com>
2020-07-21 15:54:48 +0700

    berhasil menampilkan Detail

--- latihan_react_clara/src/App.js
+++ latihan_react_clara/src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import BoostrapComp from './Component/Class/BoostrapComp';
+//import BoostrapComp from './Component/Class/BoostrapComp';
 import Home from './Component/Fungsional/Home';
 import Login from './Component/Class/Login';
+import NavbarComp from './Component/Fungsional/NavbarComp'
+import HomePage from './Component/Fungsional/HomePage';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import About from './Component/Fungsional/AboutComp';
+import CardComp from './Component/Fungsional/CardComp'
+import DetailComp from './Component/Fungsional/DetailComp';
+
 //import logo from './logo.svg';
 //import Home from './Component/Fungsional/Home';
 //import Beranda from './Component/Class/Beranda';
 //import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <BrowserRouter>
+    <NavbarComp/>
       <Home/>
       <Login/>
-       <BoostrapComp/>
-    </div>
+      <switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/detail/:id" component={DetailComp}/>
+        </switch>
+    </BrowserRouter>
+      
+      
+    
   );
 }
 
